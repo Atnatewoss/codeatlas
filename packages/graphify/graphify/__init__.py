f979f60130a1dfa3be4a1f90922b03aef1c93990
atnatewoss <icodeyt21@gmail.com>
2026-06-25 03:38:07 +0300

    feat: remove legacy graphify package

--- packages/graphify/graphify/__init__.py
+++
@@ -1,3 +0,0 @@
-from graphify.code_graph import CodeGraph, get_or_build_graph
-
-__all__ = ["CodeGraph", "get_or_build_graph"]
--- packages/graphify/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="codeatlas-graphify",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=["graphifyy>=0.8.44", "networkx>=3.0"],
-)
